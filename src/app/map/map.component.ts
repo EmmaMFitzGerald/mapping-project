56cef582abf94a8bfac97f2bbdbb3707ee2554f3
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-04-21 13:47:45 -0400

    added numberDate

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,7 +3,6 @@ import { MapService } from '../map.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { async } from '@angular/core/testing';
 
-declare let mapboxgl:any;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -11,41 +10,11 @@ declare let mapboxgl:any;
   providers: [MapService]
 })
 export class MapComponent implements OnInit {
-
-  data;
-
-  constructor(private mapService: MapService) { }
-
-async ngOnInit(): Promise<any> {
-
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZW1tYW1maXR6Z2VyYWxkIiwiYSI6ImNrOTA5bmFiOTIxcXAzZnFxdWt1OXJ6d3YifQ.fOuVi7c-DSQIGwXyrqb8ZQ';
-    const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 3,
-    center: [-98, 38]
-    });
-
-    map.on('style.load', function() {
-
-      map.addLayer({
-        id: 'coronaData',
-        type: 'circle',
-        source: {
-          type: 'geojson',
-          data: "../assets/corona-geojson.geojson" // replace this with the url of your own geojson
-        },
-        'paint': {
-          'circle-color': [
-          'interpolate',
-          ['linear'],
-          ['get', 'deaths'],
-          6,
-          '#FCA107',
-          8,
-          '#7F3121'
-          ],
-
-      }
-    })
-})}}
+  
+  constructor(private mapService: MapService) {
+   }
+   
+  ngOnInit(): void {
+    this.mapService.ngOnInit()
+  }
+}
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { MapService } from '../map.service';
 import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-slider',
@@ -15,35 +16,18 @@ export class SliderComponent implements OnInit {
   
   constructor(private mapService: MapService) { }
 
-  ngOnInit() {
-    
-    // this.createSliderLabel();
+  filterDate(){
+    console.log("filtered")
 
+    const element = event.currentTarget as unknown as HTMLInputElement
+    const date = element.value
+    console.log("date", date)
+    this.mapService.setDateToFilter(date);
   }
 
-
-
-  // private createSliderLabel(){
-  //   this.mapService.coronaData$
-  //   .subscribe(res => {
-  //     this.data = res
-
-  //     this.coronaGeoJson = {}
-
-  //     res.forEach(element => {
-
-  //       this.coronaGeoJson[res.indexOf(element)] = {
-  //         "properties": element,
-  //         "type": "Feature",
-  //         "geometry": {
-  //           "type": "Point",
-  //           "coordinates": [1, 2],
-  //         }
-
-  //       }
-  //     });
-  //     console.log("coronageojson", this.coronaGeoJson)
-  //   })
+  ngOnInit() {
+    
+  }
 }
   
 
