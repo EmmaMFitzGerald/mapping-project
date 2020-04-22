281d6d97bc2826d2a0d522de515a3200777162f7
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-04-22 16:32:13 -0400

    eslint

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ElementRef } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Subject, BehaviorSubject } from "rxjs";
+import { HttpClient } from '@angular/common/http';
 
 declare let mapboxgl:any;
 
@@ -6,63 +9,155 @@ declare let mapboxgl:any;
   providedIn: "root",
 })
 export class MapService {
+  coronaData$ = new BehaviorSubject<any>(undefined);
+
+  selectedDateData$ = new Subject<any>();
+
+  constructor(private http: HttpClient) { 
+    this.getCoronaData();
+  }
 
   public map:any
 
-  setCaseDateToFilter(numberDate){
+  setDateToFilter(numberDate: string) {
     this.map.setFilter('cases', ['==', ['number', ['get', 'numberDate']], parseInt(numberDate)]);
-  }
-  setDeathDateToFilter(numberDate){
     this.map.setFilter('deaths', ['==', ['number', ['get', 'numberDate']], parseInt(numberDate)]);
+
+    // Tell subsscirbers to selectedDateData$ the total to date
+    this.selectedDateData$.next({
+      totalDeaths: this.updateDeath(parseInt(numberDate)),
+      totalCases: this.updateCase(parseInt(numberDate))
+    });
+
   }
 
-  async ngOnInit(): Promise<any> {
-    
+  getCoronaData(){
+   return this.http.get("../assets/corona-geojson.geojson")
+      .subscribe((originalData: any) => {
+
+        console.log("OriginalData is", originalData);
+
+        // Set the replay subject to the originalData value
+        this.coronaData$.next(originalData);
+      });
+  }
+
+  updateDeath(numberDate: number) {
+
+    const coronaData = this.coronaData$.getValue();
+
+        let total = [];
+
+        let array = coronaData["features"]
+
+        array.forEach(element => {
+          if (element.properties.numberDate === numberDate){
+            if (element.properties.deaths !== 0){
+            total.push(element.properties.deaths)
+          }}
+        })
+
+        let newTotal = 0
+
+        for(let i=0; i < total.length; i++){
+          newTotal = newTotal + Number(total[i]);
+        }
+        return newTotal; // = this.selectedDateData
+  }
+
+  updateCase(numberDate: number) {
+
+    const coronaData = this.coronaData$.getValue();
+
+        let total = [];
+
+        let array = coronaData["features"]
+
+        console.log(array)
+
+        array.forEach(element => {
+          if (element.properties.numberDate === numberDate){
+            if (element.properties.cases !== 0){
+            total.push(element.properties.cases)
+          }}
+        })
+
+        let newTotal = 0
+
+        for(let i=0; i < total.length; i++){
+          newTotal = newTotal + Number(total[i]);
+        }
+        console.log("case total", total)
+        console.log("total cases:", newTotal)
+        return newTotal; // = this.selectedDateData$
+  
+  }
+
+
+   public initialize(containerElement: ElementRef) {
+
+    console.log("containerElement", containerElement.nativeElement)
+
+    const coronaData = this.coronaData$.value;
+
+    console.log("coronaData", coronaData)
+
     mapboxgl.accessToken = 'pk.eyJ1IjoiZW1tYW1maXR6Z2VyYWxkIiwiYSI6ImNrOTA5bmFiOTIxcXAzZnFxdWt1OXJ6d3YifQ.fOuVi7c-DSQIGwXyrqb8ZQ';
     this.map = new mapboxgl.Map({
-    container: 'map',
+    container: "map",
     style: 'mapbox://styles/mapbox/streets-v9',
     zoom: 3,
     center: [-98, 38]
     });
 
     this.map.on('style.load', () => {
+      console.log("inside map", coronaData)
+
+      this.map.addSource('corona-geojson', {
+        type: 'geojson',
+        data: coronaData
+      });
 
       this.map.addLayer({
         id: 'cases',
         type: 'circle',
-        source: {
-          type: 'geojson',
-          data: "../assets/corona-geojson.geojson"
-        },
+        // source: {
+        //   type: 'geojson',
+        //   data: coronaData // "../assets/corona-geojson.geojson"
+        // },
+        source: "corona-geojson",
         filter: ['==', ['number', ['get', 'numberDate']], 18281],
         paint: {
         'circle-color': [
           'step',
           ['get', 'cases'],
-          '#7FFFD4',
+          '#4d4dff',
           10,
-          '#7FFFD4',
+          '#612dba',
           20,
-          "#7FFFD4",
+          "#5a2dba",
           30,
-          '#7FFFD4',
+          '#4e2dba',
           40,
-          '#7FFFD4',
+          '#452dba',
           50,
-          '#7FFFD4',
-          60,
-          '#7FFFD4',
-          70,
-          '#7FFFD4',
-          80,
-          '#7FFFD4',
-          90,
-          '#7FFFD4',
+          '#392dba',
+          100,
+          '#2f2dba',
+          200,
+          '#2d36ba',
+          300,
+          '#2d3dba',
+          400,
+          '#0a175c',
+          500,
+          "#00001a"
           ],
           'circle-radius': [
           'step',
           ['get', "cases"],
+          0,
+          2,
           5,
           10,
           7,
@@ -74,33 +169,28 @@ export class MapService {
           13,
           50,
           15,
-          60,
-          17,
-          70,
-          19,
-          80,
-          21,
-          90,
-          23,
           100,
-          25,
-          120,
-          27,
-          130,
-          29,
-          140,
+          17,
+          200,
+          19,
+          300,
+          21,
+          400,
+          23,
+          500,
           31
-          ],  'circle-opacity': 0.65,
+          ],  'circle-opacity': 0.8,
         }
       });
 
       this.map.addLayer({
         id: 'deaths',
         type: 'circle',
-        source: {
-          type: 'geojson',
-          data: "../assets/corona-geojson.geojson"
-        },
+        // source: {
+        //   type: 'geojson',
+        //   data: coronaData // "../assets/corona-geojson.geojson"
+        // },
+        source: "corona-geojson",
         filter: ['==', ['number', ['get', 'numberDate']], 18281],
         paint: {
         'circle-color': [
@@ -129,6 +219,8 @@ export class MapService {
           'circle-radius': [
           'step',
           ['get', 'deaths'],
+          0,
+          1,
           5,
           10,
           7,
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,38 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MapService } from '../map.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { async } from '@angular/core/testing';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
 })
-export class MapComponent implements OnInit {
-  
-  constructor(private mapService: MapService) {
-   }
-   
-  filterCaseDate(){
-    const element = event.currentTarget as unknown as HTMLInputElement
-    const date = element.value
-    this.mapService.setCaseDateToFilter(date);
-    const formattedDate = new Date(((((parseInt(date)*1000)*60)*60)*24))
-    document.getElementById('caseDate').innerHTML = formattedDate.toDateString()
-  }
 
-  filterDeathDate(){
+export class MapComponent implements OnInit {
+  @ViewChild("map", { static: true }) mapElement: ElementRef;
+
+  totalDeaths: number = 0;
+  totalCases: number = 0
+  date
+
+  constructor(private mapService: MapService) {};
+   
+  filterDate(){
     const element = event.currentTarget as unknown as HTMLInputElement
     const date = element.value
-    this.mapService.setDeathDateToFilter(date);
+    this.mapService.setDateToFilter(date);
     const formattedDate = new Date(((((parseInt(date)*1000)*60)*60)*24))
-    document.getElementById('deathDate').innerHTML = formattedDate.toDateString()
+    this.date = formattedDate.toDateString()
   }
 
   ngOnInit(): void {
-    this.mapService.ngOnInit();
+    this.mapService.coronaData$.subscribe((val: any) => { 
+     if(val !== undefined){
+       this.mapService.initialize(this.mapElement) 
+        console.log(this.mapElement)
+      } else {
+        this.mapService.initialize(this.mapElement) 
+        console.log(this.mapElement)
+      }
+    })
+
+    this.mapService.selectedDateData$.subscribe((val: any) => {
+      // val should have totalDeaths property
+      console.log("val:", val);
+      this.totalDeaths = val.totalDeaths.toLocaleString()
+      this.totalCases = val.totalCases.toLocaleString()
+    })
     const formattedDate = new Date(((((18283)*1000)*60)*60)*24)
-    document.getElementById('caseDate').innerHTML = formattedDate.toDateString()
-    document.getElementById('deathDate').innerHTML = formattedDate.toDateString()
+    this.date = formattedDate.toDateString()
   }
-}
+  }
